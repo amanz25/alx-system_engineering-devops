01710b18e514cffbd35732540699b4ac243d6cc5
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-08-09 05:38:08 -0700

    mandatory tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
+    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    req = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    q = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    val = req.get("data", {}).get("subscribers", 0)
-    return val
+    res = q.get("data", {}).get("subscribers", 0)
+    return res
